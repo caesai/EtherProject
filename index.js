d874f113015457a5b5e4b6f81033d6436ba5d6b6
Vasily Kharlamov <caesai@yandex.ru>
2017-08-21 20:29:24 +0300

    Blank project

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './src/App';
+
+const node = (
+  <App />
+);
+
+ReactDOM.render(
+  node,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    publicPath: '/build/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          query  :{
+              presets:['react','es2015']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+            {
+              loader: 'file-loader?name=/img/[name].[ext]'
+            }
+          ]
+      },
+      {
+        test: /\.(otf|eot|svg|ttf|woff|woff2)$/,
+        use: ['url-loader?limit=100&name=fonts/[name].[ext]']
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ]
+};
+
+module.exports = config;
